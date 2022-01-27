4de20e39493dc2a0370ae546c12f6a923b8f812a
paval_miatlitski <paval.escoba@gmail.com>
2022-01-28 01:28:16 +0300

    feat: implements player and streaming

--- client/src/components/streams/StreamShow.jsx
+++ client/src/components/streams/StreamShow.jsx
@@ -1,37 +1,61 @@
 import React from "react";
-import {connect} from 'react-redux';
-import {fetchStream} from '../../actions';
-import flv from 'flv.js';
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
+import flv from "flv.js";
 
-import Spinner from '../Spinner';
+import Spinner from "../Spinner";
 
 class StreamShow extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.videoRef = React.createRef();
   }
-  componentDidMount () {
-    this.props.fetchStream(this.props.match.params.id)
-  };
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    const { id } = this.props.match.params;
+
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
 
   render() {
-    if(!this.props.stream){
-      return <Spinner />
+    if (!this.props.stream) {
+      return <Spinner />;
     }
 
-    const {title, description} = this.props.stream;
+    const { title, description } = this.props.stream;
     return (
       <div className="col-lg-6">
-        <video ref={this.videoRef} style={{width: '100%'}} controls/>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
-    )
-  };
-};
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
-  return {stream: state.streams[ownProps.match.params.id]};
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, {fetchStream})(StreamShow);
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
--- rtmpserver/index.js
+++ rtmpserver/index.js
@@ -1,17 +1,17 @@
-const NodeMediaServer = require('node-media-server');
+const NodeMediaServer = require("node-media-server");
 
 const config = {
   rtmp: {
-    port: 1935,
+    port: 8000,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
-    ping_timeout: 60
+    ping_timeout: 60,
   },
   http: {
     port: 8000,
-    allow_origin: '*'
-  }
+    allow_origin: "*",
+  },
 };
 
 var nms = new NodeMediaServer(config);
